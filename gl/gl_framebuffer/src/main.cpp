c7cde385f380f3435014f40dc67fa431f7e7c92f
Miguel Palhas <mpalhas@gmail.com>
2013-03-08 02:50:59 +0000

    added two viewports and scissors to gl_framebuffer sample

--- gl/gl_framebuffer/src/main.cpp
+++ gl/gl_framebuffer/src/main.cpp
@@ -7,14 +7,41 @@
 
 #include <beast/debug.hpp>
 #include <beast/profile.hpp>
-
 #include <beast/gl/async_window.hpp>
-#include "config.h"
+
+#include <GL/freeglut.h>
+#include <string>
+#include <stdio.h>
+using std::string;
+
+class sample_window : public beast::gl::async_window {
+public:
+	sample_window(const string name, uint w, uint h)
+	: beast::gl::async_window(name, w, h) { }
+
+	void render() {
+		glMatrixMode(GL_PROJECTION);
+		glEnable(GL_SCISSOR_TEST);
+
+		glViewport(0, 0, 400, 300);
+		glScissor(0, 0, 400, 300);
+		glLoadIdentity();
+		glClearColor(1.0f, 0.0f, 0.0f, 1.0f );
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+
+		glViewport(400, 300, 400, 300);
+		glScissor(400, 300, 400, 300);
+		glLoadIdentity();
+		glClearColor(0.0f, 1.0f, 0.0f, 1.0f );
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	}
+};
 
 #include <iostream>
 int main(int argc, char **argv) {
 
-	beast::gl::async_window window;
+	sample_window window(string("name"), 800, 600);
 	window.start();
 	window.join();
 }
